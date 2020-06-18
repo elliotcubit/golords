903f1d59e12253371e32dfc977fbcc6c8bc97f93
Elliot Cubit <elliotcubit@gmail.com>
2020-06-18 18:40:04 -0500

    wtf?

--- handlers/create.go
+++ handlers/create.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+  "fmt"
+  
   "golords/handlers/create/handler"
   "golords/handlers/create/ping"
   "github.com/bwmarrin/discordgo"
@@ -17,9 +19,13 @@ func OnMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
     return
   }
 
+  fmt.Println("In message create")
+
   // Run appropriate command, if there is one
   for _, handler := range commandPrompts {
+    fmt.Printf("Checking %v\n", handler)
     if handler.Should(m.Content) {
+      fmt.Println("Should")
       handler.Do(s, m)
     }
   }
