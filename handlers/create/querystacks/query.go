03b6c48238f6a02f4157290cfdb467b0ef115c27
Elliot Cubit <elliotcubit@gmail.com>
2020-07-13 00:00:25 -0500

    Trivial query for stacks

---
+++ handlers/create/querystacks/query.go
@@ -0,0 +1,61 @@
+package querystacks
+
+import (
+  "strings"
+  "fmt"
+
+  pp "golords/plusplus"
+  "golords/handlers/create/handler"
+
+  "github.com/bwmarrin/discordgo"
+)
+
+func New() handler.CreateHandler {
+  return QueryHandler{}
+}
+
+type QueryHandler struct {
+  handler.DefaultHandler
+}
+
+func (h QueryHandler) Do(s *discordgo.Session, m *discordgo.MessageCreate){
+  data := strings.SplitN(m.Content, 2)
+
+  var out string
+  var err error
+  switch data[0] {
+  case "!topstacks":
+    out, err = pp.TopQuery()
+  case "!mystacks":
+    out, err = pp.SomeoneQuery([m.Author])
+  case "!stacks":
+    out, err = pp.PeopleQuery(m.Mentions)
+  default:
+    err = fmt.Errorf("Bad command")
+  }
+
+  // Mongo machine broke
+  if err != nil {
+    return
+  }
+
+  s.ChannelMessageSend(m.ChannelID, out)
+}
+
+func (h QueryHandler) GetPrompts() []string {
+  return []string{"!topstacks", "!mystacks", "!stacks"}
+}
+
+func (h QueryHandler) Help() string {
+  return "Find out how many stacks someone has.\n!topstacks: top 5\n!mystacks: your stacks\n!stacks @someone: someone elses stacks"
+}
+
+func (h QueryHandler) Should(hint string) bool {
+  prompts := h.GetPrompts()
+  for _, v := range prompts {
+    if strings.HasPrefix(hint, v) {
+      return true
+    }
+  }
+  return false
+}
--- plusplus/plusplus.go
+++ plusplus/plusplus.go
@@ -4,10 +4,13 @@ import (
   "os"
   "log"
   "context"
+  "fmt"
 
   "go.mongodb.org/mongo-driver/mongo"
   "go.mongodb.org/mongo-driver/mongo/options"
   "go.mongodb.org/mongo-driver/bson"
+
+  "github.com/bwmarrin/discordgo"
 )
 
 // Called when module first loaded
@@ -25,6 +28,51 @@ type Info struct {
 
 var client mongo.Client
 var collection *mongo.Collection
+
+func PeopleQuery(users []*discordgo.User) (string, error) {
+  if len(users) == 0 {
+    return "", fmt.Errorf("No users provided")
+  }
+
+  var users_query []string
+  for _, user := range users {
+    users_query = append(users_query, user.String())
+  }
+
+  filter := bson.M{
+    "user": bson.M{
+      "$in": users_query,
+    },
+  }
+
+  cursor, err := collection.Find(context.TODO(), filter)
+  if err != nil {
+    // TODO all these people actually just have zero stacks
+    return "", fmt.Errorf("No documents")
+  }
+
+  var results []bson.M
+  if err := cursor.All(context.TODO(), &results); err != nil {
+    return "", fmt.Errorf("Error unmarshalling results")
+  }
+
+  outStr := ""
+  for _, result := range results {
+    if err != nil {
+      return "", fmt.Errorf("Something awful happened")
+    }
+    score := result["score"].(int32)
+    name := result["user"].(string)
+    outStr = outStr + fmt.Sprintf("%v has %d stacks\n", name, score)
+  }
+
+  return outStr, nil
+}
+
+// TODO look into $max with $limit to solve this problem
+func TopQuery() (string, error) {
+  return "", fmt.Errorf("Not implemented lol fuck you")
+}
 
 func LoadPlusPlus() error {
   URI := os.Getenv("MONGO_URI")
